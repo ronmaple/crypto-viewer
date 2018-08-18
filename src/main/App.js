ac9658c2751b1d056db1ff76194412e92d41859b
Ron Mapue <ronmapue@gmail.com>
2018-08-18 10:57:59 -0700

    retry https

--- src/main/App.js
+++ src/main/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     }
     async componentDidMount() {
         try {
-            let url = 'http://https://cryptoprice-view-rm.herokuapp.com/' ||'http://localhost:8082/api'; 
+            let url = 'https://https://cryptoprice-view-rm.herokuapp.com/' ||'http://localhost:8082/api'; 
             const res = await fetch(url);
             
             const data = await res.json();
