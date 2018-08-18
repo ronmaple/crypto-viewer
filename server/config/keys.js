b9dd838aefdc644505bbcfced244496d3fda0457
Ron Mapue <ronmapue@gmail.com>
2018-08-18 11:28:36 -0700

    retry

---
+++ server/config/keys.js
@@ -0,0 +1,6 @@
+const keys = {
+    coinApi: 'B9DADB87-F4E3-45DE-85F0-4D0CD76675C3',
+    mongoURI: 'mongodb://ronmaple:latiosburn123@ds113732.mlab.com:13732/cryptotrack'
+};
+
+module.exports = keys;
